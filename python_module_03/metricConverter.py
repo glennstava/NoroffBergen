7ea22869fea690d1335d8376b3cbac14f21ec2cb
Glenn Stava <glenn.stava@gmail.com>
2023-12-05 20:53:12 +0100

    change in metric converter

---
+++ python_module_03/metricConverter.py
@@ -0,0 +1,12 @@
+# Create a script called MetricConverter.py. 
+# In the script, create a function called metric_converter, 
+# which receives a single numeric argument. 
+
+# The purpose of this method is to receive a value in inches and then 
+# convert it to centimetres. The function should display the number 
+# of inches that it received and the number of centimetres after conversion. 
+# The function shouldn’t return any values. 
+
+# Demonstrate the use of the function in the main section of your script.
+# --------------------------------------------------------------------------
+
